7a6f2975b2fbaf076344a46a99a86cdaf0ee504e
kthakk4 <16655094+kthakk4@users.noreply.github.com>
2020-01-25 20:39:11 -0800

    Added requirements.txt and setup.py for building distribution package

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="retention", # Replace with your own username
+    version="0.0.1",
+    author="Kushan Thakkar",
+    author_email="kthakk4@gmail.com",
+    description="A package to model customer retention",
+    url="https://github.com/kthakk4/retention",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
